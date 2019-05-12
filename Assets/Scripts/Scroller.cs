36ecf6a35751161a21bd41f13c0792dff54b8e4b
GurenMK <aurbanyak@gmail.com>
2019-05-11 20:30:54 -0400

    initial commit

---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scroller : MonoBehaviour
+{
+
+	public bool CanScroll;
+
+	private Renderer _renderer;
+	public float _speed = 0.0125f; //current scrolling speed
+	private float _desiredSpeed; //desired scrolling speed
+	private float _desiredSpeed2; //half speed
+	private float _pos; //total texture offset
+	private float _lastChange; //just for demonstration
+
+	protected void Awake()
+	{
+		_renderer = GetComponent<Renderer>();
+		CanScroll = true;
+
+		_desiredSpeed = _speed;
+		_lastChange = 0; //just for demonstration
+	}
+
+	protected void Update()
+	{
+		if (!CanScroll) return;
+		/*
+		//just for demonstration: change desired speed
+		if (Time.time > _lastChange + 2) {
+			_desiredSpeed = Random.Range(-1f, 0f);
+			_lastChange = Time.time;
+			Debug.Log ("Set Speed To " + _desiredSpeed);
+		}
+		*/
+
+		//this is where the magic happens: easing by hand!
+		//Play with the value 0.01f to speed up or slow down the easing. 
+		//_speed += (_desiredSpeed - _speed) * 0.01f;
+
+		//add current speed to the position
+
+		_pos += _speed * Time.deltaTime;
+
+		//for me it works without Mathf.Repeat
+		//float y = Mathf.Repeat(_pos, 1);
+		Vector2 offset = new Vector2(_pos, 0);
+		_renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
---
+++ Assets/Scripts/TextScroll.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScroll : MonoBehaviour {
+
+
+	public GUIStyle potionStyle;
+
+	int GUIDepthInt = -1;
+
+	Vector3 matrixVector;
+
+	float native_width = 1280;
+	float native_height = 720;
+
+	float rx;
+	float ry;
+
+	int textUpCounter;
+
+	void Start()
+	{
+		//scale for your matrix
+		//this is where things get messy at different aspect ratios for your scale
+		//if you always use a specific native width and height like 1280, 720
+		rx = Screen.width/native_width;
+		ry = Screen.height/native_height;
+
+		matrixVector = new Vector3 (rx, ry, 1);
+
+		potionStyle.normal.textColor = Color.red;
+
+		textUpCounter = 0;
+	}
+
+	void OnGUI()
+	{
+
+
+			GUI.depth = GUIDepthInt;
+
+			GUI.matrix = Matrix4x4.TRS (new Vector3(0, 0, 0), Quaternion.identity, matrixVector); 
+
+			++textUpCounter;
+
+			//this will center your text if you use the GUIStyle with "Upper Center" allignment
+			//with the 1280, 720 formatting matrix
+			GUI.Label (new Rect (640, 140 - textUpCounter, 0, 0), "+1 Health Potions!", potionStyle);
+
+
+	}
+}
